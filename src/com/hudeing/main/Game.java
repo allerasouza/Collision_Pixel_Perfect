102bace58ceb18fa750d399c6e3055fb167927e0
Aller Augusto de Souza <aller.souza@samsung.com>
2020-02-24 20:37:05 -0300

    Initial commit

---
+++ src/com/hudeing/main/Game.java
@@ -0,0 +1,115 @@
+package com.hudeing.main;
+
+import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.Cursor;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.FontFormatException;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.Point;
+import java.awt.Toolkit;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferInt;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
+import javax.imageio.ImageIO;
+import javax.swing.JFrame;
+
+public class Game extends Canvas implements Runnable, MouseListener {
+
+	public static int WIDTH = 480;
+	public static final int HEIGHT = 480;
+	
+	public Game() {
+		this.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+		this.addMouseListener(this);
+	}
+	
+	public void update() {
+		
+	}
+	
+	public void render() {
+		BufferStrategy bs = this.getBufferStrategy();
+		if(bs == null) {
+			this.createBufferStrategy(3);
+			return;
+		}
+		Graphics g = bs.getDrawGraphics();
+		g.setColor(new Color(0, 0, 0));
+		g.fillRect(0, 0, WIDTH, HEIGHT);
+		
+		g.dispose();
+		bs.show();
+	}
+	
+	public static void main(String[] args) {
+		Game game = new Game();
+		JFrame frame = new JFrame();
+		frame.setTitle("Collision Pixel Perfect");
+		frame.add(game);
+		frame.setResizable(false);
+		frame.pack();
+		frame.setLocationRelativeTo(null);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		frame.setVisible(true);
+		new Thread(game).start();
+	}
+
+	@Override
+	public void run() {
+		double fps = 60.0;
+		while(true) {
+			update();
+			render();
+			try {
+				Thread.sleep((int)(1000 / fps));
+			} catch (InterruptedException e) {}
+		}
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+}
